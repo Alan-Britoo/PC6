e090979eb835b3a321765e0d2cbdc12e2d246416
Alan-Britoo <rotcarrillo_8@hotmail.com>
2024-01-23 17:15:21 -0500

    falta darle estilo a las ultimas imagenes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Gallery from './Components/Gallery'
+import TestimonioalContainer from './Components/TestimonioalContainer'
+
+function App() {
+  
+
+  return (
+    <>
+     
+     <TestimonioalContainer/>
+     <Gallery/>
+    </>
+    
+  )
+}
+
+export default App
